775c85f54fd1b1cc20fb0c25eb0ec62ba8afd7d6
Lukino <LukinoProg@gmail.com>
2025-12-06 22:08:47 +0100

    feat: add pretty table to display all attempts

---
+++ src/components/attempts-table.tsx
@@ -0,0 +1,79 @@
+import { For, createMemo } from "solid-js";
+import type { Attempt } from "../core/types/attempt";
+import { css } from "@emotion/css";
+import { defineComponent } from "../tools/utils";
+import { format } from "date-and-time";
+
+const sTextAlignCenter = css({
+  textAlign: "center",
+});
+
+interface AttemptsTableProps {
+  attempts: Array<Attempt>;
+}
+
+export const AttemptsTable = defineComponent<AttemptsTableProps>((props) => {
+  const laps = createMemo(() =>
+    Array.from({ length: Math.max(...props.attempts.map((attempt) => attempt.laps)) }, (_v, index) => index + 1),
+  );
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th rowspan="2" class={sTextAlignCenter}>
+            Date
+          </th>
+          <th rowspan="2" class={sTextAlignCenter}>
+            Map
+          </th>
+          <th colspan="2" class={sTextAlignCenter}>
+            Total
+          </th>
+          <For each={laps()}>
+            {(_, index) => <th colspan="3" class={sTextAlignCenter}>{`Split ${index() + 1}`}</th>}
+          </For>
+        </tr>
+        <tr>
+          <th rowSpan="2" class={sTextAlignCenter}>
+            Time
+          </th>
+          <th rowSpan="2" class={sTextAlignCenter}>
+            Coins
+          </th>
+          <For each={laps()}>
+            {() => (
+              <>
+                <th class={sTextAlignCenter}>{`Time`}</th>
+                <th class={sTextAlignCenter}>{`Shrooms`}</th>
+                <th class={sTextAlignCenter}>{`Coins`}</th>
+              </>
+            )}
+          </For>
+        </tr>
+      </thead>
+
+      <tbody>
+        <For each={props.attempts}>
+          {(attempt) => (
+            <tr>
+              <td class={sTextAlignCenter}>{format(new Date(attempt.timestamp), "YYYY.MM.DD | HH:mm:ss")}</td>
+              <td>{attempt.track}</td>
+              <td class={sTextAlignCenter}>{attempt.time ?? "-"}</td>
+              <td class={sTextAlignCenter}>{attempt.coins ?? "-"}</td>
+              <For each={laps()}>
+                {(_, index) => (
+                  <>
+                    <td class={sTextAlignCenter}>{attempt.splits.at(index())?.time ?? "-"}</td>
+                    <td class={sTextAlignCenter}>{attempt.splits.at(index())?.shrooms ?? "-"}</td>
+                    <td class={sTextAlignCenter}>{attempt.splits.at(index())?.coins ?? "-"}</td>
+                  </>
+                )}
+              </For>
+            </tr>
+          )}
+        </For>
+      </tbody>
+    </table>
+  );
+});
--- src/components/debug.tsx
+++ src/components/debug.tsx
@@ -1,3 +1,4 @@
+import { AttemptsTable } from "./attempts-table";
 import { css } from "@emotion/css";
 import { defineComponent } from "../tools/utils";
 import type { useTimeTrial } from "../compositions/use-time-trial";
@@ -16,21 +17,30 @@ export const Debug = defineComponent<DebugProps>((props) => (
   <>
     <h2>Debug</h2>
     <button onClick={props.timeTrial.start}>Start capture</button>
-    <div class={sDebug}>
-      <div>
-        <h2>Video</h2>
+    <div>
+      <h2>Video</h2>
+      <div class={sDebug}>
         {props.timeTrial.video}
         {props.timeTrial.canvas}
       </div>
-      <div>
-        <h2>State</h2>
-        <pre>
-          <code>{JSON.stringify(props.timeTrial.attempt(), undefined, 2)}</code>
-        </pre>
-        <h2>Raw data</h2>
-        <pre>
-          <code>{JSON.stringify(props.timeTrial.attempts(), undefined, 2)}</code>
-        </pre>
+
+      <AttemptsTable attempts={props.timeTrial.attempts()}></AttemptsTable>
+
+      <h2>Raw data</h2>
+      <div class={sDebug}>
+        <div>
+          <h3>Last Attempt</h3>
+          <pre>
+            <code>{JSON.stringify(props.timeTrial.attempt(), undefined, 2)}</code>
+          </pre>
+        </div>
+
+        <div>
+          <h3>All</h3>
+          <pre>
+            <code>{JSON.stringify(props.timeTrial.attempts(), undefined, 2)}</code>
+          </pre>
+        </div>
       </div>
     </div>
   </>
