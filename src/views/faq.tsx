68553eec4451dfc7d8079cb43cdf45035126b1cc
Lukino <LukinoProg@gmail.com>
2025-12-21 01:04:58 +0100

    feat: update faq

--- src/views/faq.tsx
+++ src/views/faq.tsx
@@ -8,6 +8,13 @@ export const FAQ = defineComponent(() => {
   return (
     <>
       <h1>FAQ</h1>
+
+      <h2>How data is stored?</h2>
+      <p>
+        The data is exclusively stored in your browser using IndexedDB. On some browser, you may be ask to allow
+        persistent storage.
+      </p>
+
       <h2>How can I backup all my data?</h2>
       <p>
         <button onclick={storage.download}>Export</button>
@@ -16,6 +23,12 @@ export const FAQ = defineComponent(() => {
       <h2>How can I restore all my data?</h2>
       <p>
         <ValibotImportButton onclick={storage.restore}>Import</ValibotImportButton>
+      </p>
+
+      <h2>What is codetabs.com?</h2>
+      <p>
+        codetabs.com provides a cors proxy. It allows you to retrieve data from mkwrs.com. Without, it would not be
+        possible to have the automatic updates of the world records.
       </p>
     </>
   );
