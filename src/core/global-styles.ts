91785ce6d5d254308e2310ac80936d324353e239
Lukino <LukinoProg@gmail.com>
2026-01-02 16:26:14 +0100

    feat: force usage of the dark theme

--- src/core/global-styles.ts
+++ src/core/global-styles.ts
@@ -2,6 +2,20 @@ import { injectGlobal } from "@emotion/css";
 
 injectGlobal({
   ":root": {
+    // For dark theme of simplecss
+    "color-scheme": "dark",
+    "--bg": "#212121",
+    "--accent-bg": "#2b2b2b",
+    "--text": "#dcdcdc",
+    "--text-light": "#ababab",
+    "--accent": "#ffb300",
+    "--accent-hover": "#ffe099",
+    "--accent-text": "var(--bg)",
+    "--code": "#f06292",
+    "--preformatted": "#ccc",
+    "--disabled": "#111",
+
+    // Custom css variables
     "--mk-spacing-medium": "0.5rem",
     "--mk-spacing-large": "1rem",
     "--mk-border": "var(--border-width) solid var(--border)",
