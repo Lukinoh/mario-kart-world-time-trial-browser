1b505cc70d6b1201a1af801bd9d15485605dde04
Lukino <LukinoProg@gmail.com>
2026-01-07 19:34:09 +0100

    chore: remove @blazediff algorithm because replaced by mean square root

--- src/domains/image-manipulation/image/image-similarity.ts
+++ src/domains/image-manipulation/image/image-similarity.ts
@@ -1,32 +1,9 @@
-import { type GmsdOptions, gmsd } from "@blazediff/gmsd";
-import { type HitchhikersSsimOptions, hitchhikersSSIM } from "@blazediff/ssim/hitchhikers-ssim";
-import { type MsssimOptions, msssim } from "@blazediff/ssim/msssim";
-import { type SsimOptionsExtended, ssim } from "@blazediff/ssim/ssim";
 import type { AnyArgsFunction } from "../../_core/types/any-args-function";
 import type { EnhancedImageData } from "./enhanced-image-data";
 
 export type ImageSimilarityFunction = (imageData1: EnhancedImageData, imageData2: EnhancedImageData) => number;
 
 export const ImageSimilarity = {
-  gmsd(options?: GmsdOptions): ImageSimilarityFunction {
-    return (imageData1, imageData2) =>
-      1 - gmsd(imageData1.data, imageData2.data, undefined, imageData1.width, imageData1.height, options);
-  },
-  ssim(options?: SsimOptionsExtended): ImageSimilarityFunction {
-    return (imageData1, imageData2) =>
-      ssim(imageData1.data, imageData2.data, undefined, imageData1.width, imageData1.height, options);
-  },
-  msssim(options?: MsssimOptions): ImageSimilarityFunction {
-    return (imageData1, imageData2) =>
-      msssim(imageData1.data, imageData2.data, undefined, imageData1.width, imageData1.height, options);
-  },
-  hitchhikersSSIM(options?: HitchhikersSsimOptions): ImageSimilarityFunction {
-    return (imageData1, imageData2) =>
-      hitchhikersSSIM(imageData1.data, imageData2.data, undefined, imageData1.width, imageData1.height, {
-        windowSize: 3,
-        ...options,
-      });
-  },
   mse(): ImageSimilarityFunction {
     return (imageData1, imageData2) => {
       let squareError = 0;
