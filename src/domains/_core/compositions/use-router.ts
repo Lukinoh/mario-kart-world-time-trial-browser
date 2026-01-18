b447b42c47c9ef800d1bcf9f51265ff5127eb635
Lukino <LukinoProg@gmail.com>
2026-01-19 00:00:31 +0100

    fix: replace history by hash router to fix 404 on github pages
    GitHub pages does not support SPA.
    By using the history router, we fix:
    - refresh of the page
    - back and next navigation
    - direct access to the page using the url

--- src/domains/_core/compositions/use-router.ts
+++ src/domains/_core/compositions/use-router.ts
@@ -1,11 +1,8 @@
-import * as v from "valibot";
 import {
   type HashRouterProps,
   type RouterProps as HistoryRouterProps,
   HashRouter as hashRouter,
-  Router as historyRouter,
 } from "@solidjs/router";
-import { BaseUrlSchema } from "../schemas/base-url";
 import type { JSX } from "solid-js";
 
 type RouterProps = Omit<HashRouterProps & HistoryRouterProps, "base">;
@@ -18,8 +15,6 @@ interface UseRouter {
   getUrl: (path: string) => string;
 }
 
-const baseUrl = v.parse(BaseUrlSchema, import.meta.env.BASE_URL);
-const routerBaseUrl = baseUrl.slice(0, -1);
 const isFile = location.protocol === "file:";
 
 const File: UseRouter = {
@@ -31,18 +26,26 @@ const File: UseRouter = {
   },
 };
 
-const History: UseRouter = {
-  Router: (props: RouterProps) => historyRouter({ ...props, base: routerBaseUrl }),
-  getUrl: (path: string) => {
-    const url = new URL(location.href);
-    url.pathname = `${baseUrl}${path}`;
-    return url.toString();
-  },
-};
+// Currently, GitHub pages does not support SPA... so we cannot use the History router.
+// import * as v from "valibot";
+// import { Router as historyRouter } from "@solidjs/router";
+// import { BaseUrlSchema } from "../schemas/base-url";
+// const baseUrl = v.parse(BaseUrlSchema, import.meta.env.BASE_URL);
+// const routerBaseUrl = baseUrl.slice(0, -1);
+// const Web: UseRouter = {
+//   Router: (props: RouterProps) => historyRouter({ ...props, base: routerBaseUrl }),
+//   getUrl: (path: string) => {
+//     const url = new URL(location.href);
+//     url.pathname = `${baseUrl}${path}`;
+//     return url.toString();
+//   },
+// };
+
+const Web: UseRouter = File;
 
 export function useRouter(): UseRouter {
   if (isFile) {
     return File;
   }
-  return History;
+  return Web;
 }
--- src/domains/_core/schemas/base-url.ts
+++ src/domains/_core/schemas/base-url.ts
@@ -1,6 +1,8 @@
 import * as v from "valibot";
 import { isString } from "remeda";
 
+// Currently, unused because GitHub pages does not support SPA.
+// Should be used by use-router.ts
 export const BaseUrlSchema = v.pipe(
   v.string(),
   v.custom<`/${string}/`>(
